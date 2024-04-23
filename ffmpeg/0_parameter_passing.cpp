b544474d847c1070a0b5e5cef5a58aeb47f1e252
qiaochao <qiaochao@icourt.cc>
2024-04-23 20:57:38 +0800

    feat: des

--- ffmpeg/0_parameter_passing.cpp
+++ ffmpeg/0_parameter_passing.cpp
@@ -73,7 +73,7 @@ int main (){
 
     //---TOOD，如果你传入的参数本身就是一个指针呢
     int data = 10;
-    int* ptr = &data;
+    int* ptr = &data; //指针是需要赋予的，如果没有一般先初始化为null，防止出现野指针
 
     changeValueWithPtr1(ptr);
     changeValueWithPtr2(&ptr);
