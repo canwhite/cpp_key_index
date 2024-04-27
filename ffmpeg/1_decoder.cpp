ecc53eedbf8a67908f72c107d7e1a837aa78338c
qiaochao <qiaochao@icourt.cc>
2024-04-27 22:37:24 +0800

    feat: new decoder

--- ffmpeg/1_decoder.cpp
+++ ffmpeg/1_decoder.cpp
@@ -45,105 +45,116 @@ V：色彩高度，它表示的是颜色从绿色至紫色的部分的信息。
 
 int main(){
 
-    //分配一个AVFormatContext。这是一个包含了流（视频，音频，字幕等）信息的结构。
+    //设置视频文件路径
+    const char* videoPath = "/Users/zack/Desktop/test.mp4";
+
+    // 分配一个AVFormatContext结构体
     AVFormatContext *pFormatCtx = avformat_alloc_context();
-    //视频文件的路径。
-    auto videoPath = "/Users/zack/Desktop/test.mp4";
 
-    // 打开输入流，并读取文件头。文件头包含了所有流的信息。
-    // &pFormatCtx 是一个指向AVFormatContext的指针的指针。这是因为函数可以改变pFormatCtx的值（可以被重新分配或者释放）。
-    // 这是C语言中修改指针值的通常方式。在C++中，你也可以使用引用，但是FFmpeg的API是用C编写的。
-    if(avformat_open_input(&pFormatCtx, videoPath, NULL ,NULL) != 0){
-        //todo
-        printf("Cannot open input file\n");
+    
+    if (avformat_open_input(&pFormatCtx, videoPath, NULL, NULL) != 0)
         return -1;
-    }
 
-    // 视频流的索引。这个值在后面的循环中被设置。
-    int video_stream_index = -1;
-
-    //循环只选择了第一个流作为视频流，这可能不总是正确的
-    //TODO,思考是不是这里获取的有问题
-    for(int i = 0 ; i < pFormatCtx->nb_streams; i++){
-        video_stream_index = i;
-        break;
-    }
-
-    // 如果没有找到视频流，表明文件中没有视频。
-    if (video_stream_index == -1) {
-        printf("Cannot find video stream\n");
+    // 搜索流信息
+    if (avformat_find_stream_info(pFormatCtx, NULL) < 0)
         return -1;
+
+    // 查找视频流索引
+    int videoStream = -1;
+    for (int i = 0; i < pFormatCtx->nb_streams; i++) {
+        if (pFormatCtx->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
+            videoStream = i;
+            break;
+        }
     }
-
-    //获取解码器参数。这里streams[video_stream_index]->codecpar可能含有解码器需要的额外信息，比如帧率、时间基准等。
-    AVCodecParameters *pCodecPar =  pFormatCtx->streams[video_stream_index]->codecpar;
-    int pixel_format_int = pCodecPar->format;
-
-    //获取像素格式
-    AVPixelFormat pixel_format = static_cast<AVPixelFormat>(pixel_format_int);
-    printf("--pix %d /n",pixel_format); //这里输出为-1，有问题
+    // 没有找到视频流
+    if (videoStream == -1)
+        return -1;
 
 
-    // 找到对应的解码器。
-    // codec_id 是解码器的标识符，对每种解码器来说都是唯一的。
+    //从视频流中获取指定视频流（videoStream）的编解码器参数，
+    AVCodecParameters *pCodecPar = pFormatCtx->streams[videoStream]->codecpar;
+
+    //根据编解码器参数中的编解码器ID（codec_id）在FFmpeg的编解码器列表中查找对应的解码器
     const AVCodec *pCodec = avcodec_find_decoder(pCodecPar->codec_id);
 
-    // 如果没有找到解码器。
+
+    //判断解码器是否成功找到
     if (pCodec == NULL) {
         printf("Cannot find codec\n");
         return -1;
     }
 
-    // 分配一个解码器上下文。这个上下文在解码过程中，用来保存解码器的状态。
+    // 分配一个解码器上下文
     AVCodecContext *pCodecCtx = avcodec_alloc_context3(pCodec);
 
-    // 如果没能分配上下文。
-    if (pCodecCtx == NULL) {
-        printf("Cannot allocate memory for codec context\n");
-        return -1;
-    }
-
-    // 把参数从AVCodecParameters复制到AVCodecContext。这是因为解码函数需要AVCodecContext。
     if (avcodec_parameters_to_context(pCodecCtx, pCodecPar) < 0) {
         printf("Failed to copy codec parameters to codec context\n");
         return -1;
     }
 
+    //打开解码器
+    if (avcodec_open2(pCodecCtx, pCodec, NULL) < 0) {
+        printf("Failed to open codec\n");
+        return -1;
+    }
 
-    //TODO, 如何将pCodecCtx解码器上下文的内容，转成YUV图像
-    // 分配一个AVFrame结构体，用于存储未解码的图像帧
-    AVFrame* pFrameYUV = av_frame_alloc();
-    // 获取所提供的图像格式和大小所需的内存大小（以字节为单位）
-    int numBytes = av_image_get_buffer_size(AV_PIX_FMT_YUV420P, pCodecCtx->width, pCodecCtx->height, 1);
-    // 分配一块内存（以字节为单位）
-    uint8_t* buffer = (uint8_t*)av_malloc(numBytes * sizeof(uint8_t));
+    // 分配一个AVFrame结构体
+    AVFrame *pFrame = av_frame_alloc();
+    AVPacket packet;
 
-    // 根据提供的参数设置data和linesize数组
-    av_image_fill_arrays(pFrameYUV->data, pFrameYUV->linesize, buffer, AV_PIX_FMT_YUV420P, pCodecCtx->width, pCodecCtx->height, 1);
+    // 初始化像素格式转换上下文
+    struct SwsContext *swsContext = sws_getContext(pCodecCtx->width, pCodecCtx->height, pCodecCtx->pix_fmt,
+                                                   pCodecCtx->width, pCodecCtx->height, AV_PIX_FMT_YUV420P,
+                                                   SWS_BICUBIC, NULL, NULL, NULL);
+    // 打开输出文件
+    FILE *file = fopen("output.yuv", "wb");
+    if (!file) {
+        printf("Failed to open output file\n");
+        return -1;
+    }
 
-    printf("---width%d \n",pCodecCtx->width);
-    printf("---height%d \n",pCodecCtx->width);
-    printf("---scrFormat%d \n",pCodecCtx->pix_fmt); //这里像素格式输出为-1，明显有问题呀
-    printf("---tWidth%d",pCodecCtx->width);
-    // 原始数据
-    // scrW: 原始格式宽度
-    // scrH: 原始格式高度
-    // scrFormat: 原始数据格式
+    // 读取视频帧并转换为YUV
+    while (av_read_frame(pFormatCtx, &packet) >= 0) {
+        if (packet.stream_index == videoStream) {
+            avcodec_send_packet(pCodecCtx, &packet);
+            while (avcodec_receive_frame(pCodecCtx, pFrame) == 0) {
 
-    // 目标数据
-    // dstW: 目标格式宽度
-    // dstH: 目标格式高度
-    // dstFormat: 目标数据格式
-    //这个函数将返回一个SwsContext结构的指针，该结构包含了执行图像尺寸和像素格式转换所需要的所有信息。
-    // struct SwsContext* sws_ctx = sws_getContext(pCodecCtx->width, pCodecCtx->height, pCodecCtx->pix_fmt, pCodecCtx->width, pCodecCtx->height, AV_PIX_FMT_YUV420P, SWS_BICUBIC, NULL, NULL, NULL);
+                // 分配YUV帧
+                AVFrame *pFrameYUV = av_frame_alloc();
+                int numBytes = av_image_get_buffer_size(AV_PIX_FMT_YUV420P, pCodecCtx->width, pCodecCtx->height, 1);
+                uint8_t *buffer = (uint8_t *)av_malloc(numBytes * sizeof(uint8_t));
+                av_image_fill_arrays(pFrameYUV->data, pFrameYUV->linesize, buffer, AV_PIX_FMT_YUV420P, pCodecCtx->width, pCodecCtx->height, 1);
 
-    
-
-
-
-    //TODO，再然后就是下一章编码了
+                // 将视频帧数据转换为YUV格式
+                sws_scale(swsContext, pFrame->data, pFrame->linesize, 0, pCodecCtx->height, pFrameYUV->data, pFrameYUV->linesize);
 
 
+                // 写入YUV数据到文件
+                for (int i = 0; i < pCodecCtx->height; i++) {
+                    fwrite(pFrameYUV->data[0] + i * pFrameYUV->linesize[0], 1, pCodecCtx->width, file);
+                }
+                for (int i = 0; i < pCodecCtx->height / 2; i++) {
+                    fwrite(pFrameYUV->data[1] + i * pFrameYUV->linesize[1], 1, pCodecCtx->width / 2, file);
+                }
+                for (int i = 0; i < pCodecCtx->height / 2; i++) {
+                    fwrite(pFrameYUV->data[2] + i * pFrameYUV->linesize[2], 1, pCodecCtx->width / 2, file);
+                }
+
+                // 释放YUV帧资源
+                av_frame_free(&pFrameYUV);
+            }
+        }
+        av_packet_unref(&packet);
+    }
+
+    // 关闭文件和释放资源
+    fclose(file);
+    avformat_close_input(&pFormatCtx);
+    avformat_free_context(pFormatCtx);
+    avcodec_free_context(&pCodecCtx);
+    sws_freeContext(swsContext);
+    av_frame_free(&pFrame);
 
 
     return 0;
