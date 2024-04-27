b0c4ca4086cebd4184a5193f4d86c6fb4199decd
qiaochao <qiaochao@icourt.cc>
2024-04-27 22:17:01 +0800

    feat: namespace

---
+++ algorithm/sortnamespace.cpp
@@ -0,0 +1,5 @@
+namespace SortNamespace {
+    void quickSort(){
+        //todo
+    }
+}
---
+++ algorithm/sortnamespace.h
@@ -0,0 +1,8 @@
+#ifndef SORTNAMESPACE_H
+#define SORTNAMESPACE_H
+
+namespace SortNamespace {
+    void quickSort();
+}
+
+#endif
