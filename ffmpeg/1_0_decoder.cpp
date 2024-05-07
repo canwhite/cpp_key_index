3e52a694ecab5d7dee8851d439910a26ebb001bb
qiaochao <qiaochao@icourt.cc>
2024-05-07 13:55:11 +0800

    feat: init

---
+++ ffmpeg/1_0_decoder.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <future>
+#include <vector>
+#include <map>
+#include <algorithm> //find等方法
+#include <memory>
+using namespace std; //可以使用标准库里的符号和方法
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+}
+
+int main(){
+
+    return 0;
+}
