78f44c0c7e6c93318b55f94ce2da6705563dcb08
qiaochao <qiaochao@icourt.cc>
2024-05-07 15:07:55 +0800

    feat: decoder static function implement

--- ffmpeg/1_0_decoder.cpp
+++ ffmpeg/1_0_decoder.cpp
@@ -39,6 +39,33 @@ static void logging(const char *fmt, ...){
     fprintf( stderr, "\n" ); 
 }
 
+// save a frame into a .pgm file
+static void save_gray_frame(unsigned char *buf, int unit_size, int xsize, int ysize, char *filename);
+// implement
+static void save_gray_frame(unsigned char *buf, int unit_size, int xsize, int ysize, char *filename){
+    FILE * f;
+    int i;
+    f = fopen(filename,"w");
+    // writing the minimal required header for a pgm file format
+    // portable graymap format -> https://en.wikipedia.org/wiki/Netpbm_format#PGM_example
+    fprintf(f, "P5\n%d %d\n%d\n", xsize, ysize, 255);
+
+    // writing line by line
+    for (i = 0; i < ysize; i++)
+        /** 
+        size_t fwrite(const void *ptr, size_t size, size_t count, FILE *stream)
+        ptr：这是指向要写入字节的指针。
+        size：要写入的每个元素的字节大小。
+        count：要写入 size 字节的元素的数量。
+        stream：这是一个指向 FILE 对象的指针，这个对象指定了一个输出流。
+        */
+        fwrite(buf + i * unit_size, 1, xsize, f);
+
+    fclose(f);
+
+}
+
+
 // decode packets into frames
 static int decode_packet(AVPacket *pPacket, AVCodecContext *pCodecContext, AVFrame *pFrame);
 // implement, core function
@@ -69,34 +96,43 @@ static int decode_packet(AVPacket *pPacket, AVCodecContext *pCodecContext, AVFra
         }
 
         //如果没有报错
+        //可以输出 frame 编号、PTS、DTS、frame 类型等其他信息。
         if(response >= 0 ){
-            //todo
-
+            // printf(
+            //     "Frame %c (%d) pts %d dts %d key_frame %d [coded_picture_number %d, display_picture_number %d]",
+            //     av_get_picture_type_char(pFrame->pict_type),
+            //     pCodecContext->frame_number,
+            //     pFrame->pts,
+            //     pFrame->pkt_dts,
+            //     pFrame->key_frame,
+            //     pFrame->coded_picture_number,
+            //     pFrame->display_picture_number
+            // );
+            char frame_filename[1024];
+            //snprintf是一个用于格式化字符串并将其写入字符数组的函数，它来源于C标准库中的stdio.h。
+            //*str 是指向将被写入的字符数组的指针。
+            //size 是你想要写入的字符数组的最大长度，包括结尾的空字符（'\0'）。
+            //*format 是一个格式字符串，它定义了接下来的可变参数如何转换成字符串。
+            //最后一个 ... 是可变参数，它们将按照 *format 字符串所指定的格式被格式化并写入 *str。
+            // snprintf(frame_filename, sizeof(frame_filename), "%s-%d.pgm", "frame", pCodecContext->frame_number);
+            // Check if the frame is a planar YUV 4:2:0, 12bpp
+            // That is the format of the provided .mp4 file
+            // RGB formats will definitely not give a gray image
+            // Other YUV image may do so, but untested, so give a warning
+            if (pFrame->format != AV_PIX_FMT_YUV420P)
+            {
+                logging("Warning: the generated file may not be a grayscale image, but could e.g. be just the R component if the video format is RGB");
+            }
+            // save a grayscale frame into a .pgm file
+            // 因为我们只需要灰色，所以取data[0],
+            // linesize[0]通常表示的就是Y平面(亮度分量)的行大小
+            // 接着是frame的宽和高，以及要写入的文件名
+            save_gray_frame(pFrame->data[0], pFrame->linesize[0], pFrame->width, pFrame->height, frame_filename);
         }
 
     }
+    return 0;
 }
-
-
-// save a frame into a .pgm file
-static void save_gray_frame(unsigned char *buf, int wrap, int xsize, int ysize, char *filename);
-// implement
-static void save_gray_frame(unsigned char *buf, int wrap, int xsize, int ysize, char *filename){
-    //TODO,
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
