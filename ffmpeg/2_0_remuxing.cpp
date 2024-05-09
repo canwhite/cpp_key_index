503d6f0fb859ec711ac3b07d482ffb5f70e38882
qiaochao <qiaochao@icourt.cc>
2024-05-09 11:59:41 +0800

    feat: temp remuxing

--- ffmpeg/2_0_remuxing.cpp
+++ ffmpeg/2_0_remuxing.cpp
@@ -26,7 +26,7 @@ int main(){
     const char* out_filename = "remuxed_small_bunny_1080p_60fps.ts";
 
     //继续定义其他可用的属性
-    int ret,i; //ret是获取各种
+    int ret; //ret是获取各种创建结果，最后再end统一判断
     int stream_index = 0;
     int *streams_list = NULL;
     int number_of_streams = 0;
@@ -54,7 +54,59 @@ int main(){
     }
 
     //重新封装视频、音频、字幕流，因此需要将用到的这些流存入一个数组中。
+    number_of_streams = input_format_context->nb_streams;
+    //这里是给空间
+    streams_list = (int *)av_mallocz(number_of_streams * sizeof(*streams_list));
+    if (!streams_list) {
+        ret = AVERROR(ENOMEM);
+        goto end;
+    }
+
+
+    //遍历所有流，然后利用 avformat_new_stream 为每一个流创建一个对应的输出流
+    //PS：当前只需要针对视频、音频、字幕流进行处理。
+    for (int i = 0; i < input_format_context->nb_streams; i++)
+    {
+        AVStream *out_stream;
+        AVStream *in_stream = input_format_context->streams[i];
+        //参数，如果codec_type不满足视频音频和字幕这几种情况就跳过
+        AVCodecParameters *in_codecpar = in_stream->codecpar;
+        if (in_codecpar->codec_type != AVMEDIA_TYPE_AUDIO &&
+            in_codecpar->codec_type != AVMEDIA_TYPE_VIDEO &&
+            in_codecpar->codec_type != AVMEDIA_TYPE_SUBTITLE) {
+            streams_list[i] = -1;
+            continue;
+        }
+        //标记一下是第几个stream
+        streams_list[i] = stream_index++;
+        //利用 avformat_new_stream 为每一个流创建一个对应的输出流，最后的结果是挂载在output_format_context
+        out_stream = avformat_new_stream(output_format_context, NULL);
+
+        if (!out_stream) {
+            fprintf(stderr, "Failed allocating output stream\n");
+            ret = AVERROR_UNKNOWN;
+            goto end;
+        }
+
+        //然后将参数也copy过去
+        ret = avcodec_parameters_copy(out_stream->codecpar, in_codecpar);
+        
+        if (ret < 0) {
+            fprintf(stderr, "Failed to copy codec parameters\n");
+            goto end;
+        }
+
+    }
+
+    // https://ffmpeg.org/doxygen/trunk/group__lavf__misc.html#gae2645941f2dc779c307eb6314fd39f10
+    // av_dump_format。这个函数主要用于打印输入或者输出的媒体格式的详细信息，包括流、编码器、时长、比特率等等。
+    // 参数1: 流格式信息的结构体 ， 
+    // 参数2:流的索引值，希望处理的是AVFormatContext 中的第一个（索引为0）流。即第一个流（通常，索引0是视频流，索引1是音频流，当然这个也与文件的实际情况有关）
+    // 参数3:传入的媒体文件的URL
+    // 参数4:0是输出，1是输出
+    av_dump_format(output_format_context, 0, out_filename, 1);
     
+
 
 
 
