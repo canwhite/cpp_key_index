3e797420b938eaf6cb76858111c0bb5eaaa568c2
qiaochao <qiaochao@icourt.cc>
2024-05-09 19:33:34 +0800

    feat: init debug tool

---
+++ debug/video_debugging.h
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavutil/timestamp.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <libavutil/opt.h>
+#include <string.h>
+#include <inttypes.h>
+}
+
+//todo，声明方法
+
