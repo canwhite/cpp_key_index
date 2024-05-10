6f40cfb265e341c1ca57bf0d13c0aa58ba8f95b7
qiaochao <qiaochao@icourt.cc>
2024-05-10 12:54:00 +0800

    feat: log_packet

--- debug/video_debugging.cpp
+++ debug/video_debugging.cpp
@@ -8,7 +8,12 @@ void logging(const char *fmt,...){
     fprintf( stderr, "\n" );
 }
 void log_packet(const AVFormatContext *fmt_ctx, const AVPacket *pkt){
-    //
+    AVRational *time_base = &fmt_ctx->streams[pkt->stream_index]->time_base;
+    logging("pts:%s pts_time:%s dts:%s dts_time:%s duration:%s duration_time:%s stream_index:%d",
+           av_ts2str(pkt->pts), av_ts2timestr(pkt->pts, time_base),
+           av_ts2str(pkt->dts), av_ts2timestr(pkt->dts, time_base),
+           av_ts2str(pkt->duration), av_ts2timestr(pkt->duration, time_base),
+           pkt->stream_index);
 }
 void print_timing(char *name, AVFormatContext *avf, AVCodecContext *avc, AVStream *avs){
     //
