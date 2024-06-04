e6005455f4d0782a09867d2d409f947473f5e32a
qiaochao <qiaochao@icourt.cc>
2024-06-04 11:51:35 +0800

    feat: extern c

--- cpp/10_compatibility_with_c.cpp
+++ cpp/10_compatibility_with_c.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavutil/imgutils.h>
+#include <libavutil/opt.h>
+#include <libswscale/swscale.h>
+#include <libavfilter/avfilter.h>
+#include <libavfilter/buffersink.h>
+#include <libavfilter/buffersrc.h>
+}
 using namespace std;
 
 int main(){
+
+    auto inputFilePath = "/Users/zack/Desktop/test.mp4";
+    auto outputUrl = "out.mp4";
+
+    AVFormatContext *inputFormatContext = avformat_alloc_context();
     return 0;
 }
